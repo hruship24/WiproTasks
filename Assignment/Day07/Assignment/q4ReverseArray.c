9291cf5d0ebcb68e49ed5db45c774bff3d2e74b7
Hrushi <hruship24@gmail.com>
2024-03-22 16:45:45 +0530

    q4

---
+++ Assignment/Day07/Assignment/q4ReverseArray.c
@@ -0,0 +1,52 @@
+
+//Q4. reverse the array elements.
+
+#include <stdio.h>
+
+void reverseArray(int arr[], int reversed[], int num);
+void display(int arr[], int num);
+
+void reverseArray(int arr[], int reversed[], int num)
+{
+    int i, j = num - 1;
+    for (i = 0; i < num; i++)
+    {
+        reversed[i] = arr[j];
+        j--;
+    }
+}
+
+void display(int arr[], int num)
+{
+    int i;
+    for (i = 0; i < num; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+
+int main()
+{
+    int i, num;
+    int arr[num];
+    int reversed[num];
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &num);
+
+    printf("\nEnter the elements up to %d: ", num);
+    for (i = 0; i < num; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original elements are: ");
+    display(arr, num);
+
+    reverseArray(arr, reversed, num);
+
+    printf("\nReversed elements are: ");
+    display(reversed, num);
+
+    return 0;
+}
